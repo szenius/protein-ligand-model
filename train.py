fe46b68601fe37f57ccf1fa31c7d6f2b73b9a6bb
Jin Zhe <spyrant@gmail.com>
2018-10-15 04:17:10 +0800

    update training with dual stream model

--- train.py
+++ train.py
@@ -1,16 +1,18 @@
-from util import load_training_data, format_training_data
-from models import cnn
-import matplotlib.pyplot as plt
-import numpy as np
+from training_data import get_training_data
 from tensorflow import set_random_seed
 from keras import optimizers, losses
+from models import dual_stream_cnn
+import numpy as np
+import os
 
 np.random.seed(0)
 set_random_seed(0)
 
-pairs, labels = load_training_data('./training_data')
-train_x = format_training_data(pairs)
-model = cnn((len(train_x[0]), len(train_x[0][0]), len(train_x[0][0][0])))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-history = model.fit(x=train_x, y=train_y, epochs=1, verbose=1)
-print(history.history['acc'])
+def main():
+    x_protein, x_ligand, y = get_training_data()
+    model = dual_stream_cnn()
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    history = model.fit(x=[x_protein, x_ligand], y=y, epochs=1, verbose=1)
+    print(history.history['acc'])
+
+if __name__ == '__main__': main()
