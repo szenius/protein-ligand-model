a0ff9ae6aa79fb16e1fb500b336d6c8c7d6d8f6a
szenius <ting.szeying@gmail.com>
2018-10-27 22:32:34 +0800

    Complete train step

--- train.py
+++ train.py
@@ -8,12 +8,28 @@ import os
 np.random.seed(0)
 set_random_seed(0)
 
+def plot(data, labels, colours, xlabel, ylabel, title, filename):
+    plt.figure()
+    for i in range(len(data)):
+        plt.plot(data[i], label=labels[i], c=colours[i])
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.legend()
+    plt.savefig(filename, bbox_inches='tight')
+    plt.clf()
+
 def main():
     x_protein, x_ligand, y = get_training_data()
-    print(x_protein.shape(), x_ligand.shape())
+    epochs = 10
+    batch_size = 1
+    mode = 'conv3d'
     model = dual_stream_cnn()
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-    history = model.fit(x={'protein_input': x_protein, 'ligand_input': x_ligand}, y=y, epochs=1, verbose=1, batch_size=32)
-    print(history.history['loss'], history.history['acc'])
+    history = model.fit(x={'protein_input': x_protein, 'ligand_input': x_ligand}, y=y, epochs=epochs, verbose=1, batch_size=batch_size)
+        # Plot loss vs accuracy
+    plot([history.history['loss'], history.history['acc']], ['loss', 'acc'], ['b', 'r'], 'epoch', '', mode.upper()\
+    + " Training", '_'.join(['train', 'dist', mode, epochs, batch_size, '.png']))
+
 
 if __name__ == '__main__': main()
