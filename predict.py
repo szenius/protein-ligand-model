15c52376373783d125ac0522d6fd294d0f5dcd5d
szenius <ting.szeying@gmail.com>
2018-10-30 13:29:07 +0800

    Added conv to predict

--- predict.py
+++ predict.py
@@ -2,12 +2,16 @@ import sys
 from keras.models import load_model
 import numpy as np
 from predict_util import *
+from models import get_model
 
 MLP = 'mlp'
 LSTM = 'lstm'
 CONV = 'conv'
 
 TEST_DATA_PATH = os.path.abspath('./testing_data')
+OUTPUT_FILENAME = "test_predictions.txt"
+
+HEADER = '\t'.join(["pro_id", "lig1_id", "lig2_id", "lig3_id", "lig4_id", "lig5_id", "lig6_id", "lig7_id", "lig8_id", "lig9_id", "lig10_id", ])
 
 mode = sys.argv[1] # 'mlp', 'lstm', 'conv'
 
@@ -16,21 +20,28 @@ if mode == MLP:
     model = load_model('train_dist_mlp_10_32_337824.h5')
 elif mode == LSTM:
     model = load_model('train_dist_lstm_10_32.h5')
+elif mode == CONV:
+    model = get_model('Dual-stream 3D Convolution Neural Network')
+    model.load_weights('###.h5') # TODO:
 else:
-    # CONV TODO:
-    sys.exit()
+    print("Invalid mode {}".format(mode))
 print("Loaded {} model.".format(mode))
 
 # Read in all test data
-protein_data, ligand_data, max_length = load_data(TEST_DATA_PATH, -1)
+if mode == CONV:
+    protein_data, ligand_data, max_x, max_y, max_z = load_data_3d(TEST_DATA_PATH)
+else:
+    protein_data, ligand_data, max_length = load_data(TEST_DATA_PATH, -1)
 print("Loaded {} sets from testing data files.".format(len(protein_data)))
 
+# Generate predictions
 predictions = []
 output_data = np.zeros((len(protein_data), 11))
 for i in range(len(protein_data)):
     prediction_row = []
     x_protein = protein_data[i]
     print("Generating predictions for protein {}".format(i))
+
     for j in range(len(ligand_data)):
         x_ligand = ligand_data[j]
 
@@ -44,15 +55,29 @@ for i in range(len(protein_data)):
             # Predict
             result = model.predict(x)
             prediction_row.append(result[0][0])
+
         elif mode == LSTM:
             not_required, x = generate_seq_distances(x_protein, x_ligand, 4615)
             # LSTM TODO:
+
         else:
-            #CONV TODO:
-            sys.exit()
+            # Preprocessing
+            x_protein = reshape_data(x_protein, max_x, max_y, max_z)
+            x_ligand = reshape_data(x_ligand, max_x, max_y, max_z)
+
+            # Predict
+            x = {'protein_input': x_protein, 'ligand_input': x_ligand}
+            result = model.predict(x)
+            prediction_row.append(result[0][0])
     # Add top ten predictions to output_data
     prediction_row = list(reversed(sorted(prediction_row, key=float)))
     output_data[i] = prediction_row[:11]
 
 # Save to file
-np.savetxt("foo.csv", output_data, delimiter=",")
+np.savetxt(OUTPUT_FILENAME, output_data, delimiter="\t")
+
+# Write header
+with open(OUTPUT_FILENAME, 'r+') as f:
+    content = f.read()
+    f.seek(0, 0)
+    f.write(HEADER.rstrip('\r\n') + '\n' + content)
--- predict_util.py
+++ predict_util.py
@@ -7,6 +7,89 @@ import sys
 PROTEIN_FILENAME_SUFFIX = '_pro_cg.pdb'
 LIGAND_FILENAME_SUFFIX = '_lig_cg.pdb' 
 
+def reshape_data(data, max_x, max_y, max_z, num_channels=2, desc=''):
+    data_len = len(data)
+    x_len = max_x + 1
+    y_len = max_y + 1
+    z_len = max_z + 1
+    reshaped_data = np.zeros((len(data), x_len, y_len, z_len, num_channels))
+    for i in tqdm(range(data_len), desc=desc):
+        complex_seq = data[i]
+        for atom in complex_seq:
+            x = int(atom[0])
+            y = int(atom[1])
+            z = int(atom[2])
+            channel = int(atom[3])
+            reshaped_data[i][x][y][z][channel] = 1
+    return reshaped_data
+
+def load_data_3d(dir_path):
+    '''
+    Args:
+        dir_path (str): the training_data directory.
+        size of training examples in dir_path to load. 0 indicates all
+
+    Returns: 
+        protein_data (list): [ [[protein_x, protein_y, protein_z, protein_type], ... ] , ... ].
+        ligand_data (list): [ [[ligand_x, ligand_y, ligand_z, ligand_type], ... ] , ... ].
+        Where for the same index position in the respective lists, the corresponding protein and ligand is a receptive pair.
+    '''
+    ############################ Helper functions ############################
+    def get_index(filename):
+        return filename.split('_')[0]
+
+    def get_pair(index):
+        protein_path = os.path.join(dir_path, index + PROTEIN_FILENAME_SUFFIX)
+        ligand_path = os.path.join(dir_path, index + LIGAND_FILENAME_SUFFIX)
+        protein, pro_max_x, pro_max_y, pro_max_z = read_complex(protein_path)
+        ligand,  lig_max_x, lig_max_y, lig_max_z = read_complex(ligand_path)
+
+        # Resolve coordinate maxes for this pair
+        max_x = max(pro_max_x, lig_max_x)
+        max_y = max(pro_max_y, lig_max_y)
+        max_z = max(pro_max_z, lig_max_z)
+        return protein, ligand, max_x, max_y, max_z
+
+    def read_complex(file_path):
+        # Construct complex from atom data
+        complex_seq = []
+        for line in [x.strip() for x in read_lines(file_path)]:
+            x = float(line[30:38].strip())
+            y = float(line[38:46].strip())
+            z = float(line[46:54].strip())
+            atom_type = line[76:78].strip()
+            hydrophobicity = 1 if atom_type == 'C' else 0 # 1 for hydrophobic, 0 for polar
+            complex_seq.append([x, y, z, hydrophobicity])
+        
+        np_complex_seq = np.asarray(complex_seq)
+        xyzh_mins = np.amin(np_complex_seq, axis=0)                 # get all mins
+        xyzh_mins[-1] = 0                                           # force min of hydrophobicity to be 0 (just in case)
+        np_complex_seq -= xyzh_mins                                 # translate values such that min is now zero
+        max_x, max_y, max_z, _ = np.amax(np_complex_seq, axis=0)    # the new maxes for x, y, z
+        complex_seq = np_complex_seq.tolist()                       # convert back to python list
+        return complex_seq, max_x, max_y, max_z
+
+    ############################## Function body #############################
+    protein_data = []
+    ligand_data = []
+    global_max_x = 0
+    global_max_y = 0
+    global_max_z = 0
+    protein_filenames_list = ls(dir_path, lambda x: x.endswith(PROTEIN_FILENAME_SUFFIX))
+
+    for protein_filename in tqdm(protein_filenames_list, desc='Reading {} pair complexes from {}'.format(end-start if start != -1 and end != -1 else 'all', dir_path)):
+        index = get_index(protein_filename)
+        protein, ligand, max_x, max_y, max_z = get_pair(index)
+        protein_data.append(protein)
+        ligand_data.append(ligand)
+
+        # Update global maxes
+        global_max_x = max(max_x, global_max_x)
+        global_max_y = max(max_y, global_max_y)
+        global_max_z = max(max_z, global_max_z)
+
+    return protein_data, ligand_data, global_max_x, global_max_y, global_max_z
+
 def load_data(dir_path, size):
     '''
     Args:
